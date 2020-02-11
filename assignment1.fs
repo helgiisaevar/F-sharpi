f6bac7badb6893f3e4c426902f4bf84ac5cbed0b
Helgi <helgist18@ru.is>
2020-02-11 14:11:26 +0000

    Changes in assignment 1.

--- assignment1.fs
+++ assignment1.fs
@@ -25,7 +25,7 @@ let rec findSum (n: int) (xs: 'int) =
 
 let findSum2 sum xs =
     let (_, n) = 
-        List.fold (fun (r,i) -> fun x -> if r=0 then (r,i) else (t-x,i+10)) (sum,0) xs
+        List.fold (fun (r,i) -> fun x -> if r=0 then (r,i) else (r-x,i+1)) (sum,0) xs
     n
 
 
@@ -68,20 +68,30 @@ let rec count (item: string) (Listitem: string list) : int =
     | x::xs -> if item = x then 1 + count item xs else count item xs
 
 
-/// (ii)
 
+// modify : ’a -> (’a -> ’a) -> string -> (string * ’a) list -> (string * ’a) list
+let rec modify d f x xs = update x (f (lookup d x xs)) xs
+
+//(ii)
+
+
+let rec modify d f x xs =
+    match xs with 
+    | [] -> [(x, f d)]
+    | (b,h) as p :: xs -> 
+        if x = b then (x, f h) :: xs else p :: modify d f x xs
+
+
+//(iii)
 
 
 ///Assignment 5 
-
-
 let rec uf f x =
 match f x with
     | None -> []
     | Some (a, y) -> a :: uf f y
 
-let fromOne (n:int) = uf (fun x -> if (n>=x) then None else Some(x,x+1)) 1
-
+let fromOne (n : int) = uf (fun x -> if (n>=x) then Some(x,x+1) else None) 1
 
 /// Assingment 6 
 
@@ -94,9 +104,27 @@ type pos =
     | R of pos // go right
 
 
+let rec deleteSubtree (t : 'a tree) (p : pos) : 'a tree =
+    match p, t  with
+    | S, t -> Lf
+    | L p, Br (n, t1, t2) -> Br (n, deleteSubtree t1 p, t2)
+    | R p, Br (n, t1, t2) -> Br (n, t1, deleteSubtree t2 p)
+    | R p, Lf -> Lf
+    | L p, Lf -> Lf
 
-let rec deleteSubtree (n: int)
+/// Assignemnt 7 
 
+type fp = 
+    | Nil 
+    | IntCons of int * fp
+    | StrCons of string * fp 
 
+//(i)
 
+let rec  fromIntList (Listi : int list ) fp = 
+    match Listi with
+    | [] -> Nil 
+    | x :: tail fromIntList(List, tail)
+
+// (ii)
 
